ea3c9fefb02f3b0235e56566cf4da42ad8c20c96
PremanandChowdhuy <freudigprem@gmail.com>
2021-06-29 17:20:26 +0530

    fix: Small Typo fix

--- app/components/map.js
+++ app/components/map.js
@@ -1,5 +1,5 @@
 import Component from '@glimmer/component';
-import ENV from 'super-rentals/conig/environment';
+import ENV from 'super-rentals/config/environment';
 
 export default class MapComponent extends Component {
   get token() {
